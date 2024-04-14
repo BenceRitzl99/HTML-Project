e007e966b4b5f2df1c22e80919792e5ae40d7a98
Ritzl Bence <benceritzl@gmail.com>
2024-04-14 14:51:14 +0200

    Navbar Anna

--- src/app.js
+++ src/app.js
@@ -13,3 +13,17 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+function toggleNav() {
+    document.getElementById("mySidebar").style.width = 
+    document.getElementById("mySidebar").style.width === "250px" ? "0" : "250px";
+    
+}
+function backNav() {
+    var sidebar = document.getElementById("mySidebar");
+    if (sidebar.style.width === "250px") {
+      sidebar.style.width = "0";
+    }
+  }
+
+  
